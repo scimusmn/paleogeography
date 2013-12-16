745556ecc222f2c424908d5f53d751e5975db7af
bryan kennedy <bkennedy@smm.org>
2013-12-16 15:33:27 -0600

    Adding some JS to display cursor position
    Since the cursor position will dictacte the map animation, I'm starting
    out by just showing the cursor position on the screen.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$("#map").mousemove(function( event ) {
+  var map = $("#map");
 
+  var mapPos = map.position();
+
+  var mapWidth = map.width();
+  var mapHeight = map.height();
+
+  var xPos = event.pageX - mapPos.left;
+  var xPcent = ((xPos / mapWidth) * 100).toPrecision(3);
+  var yPos = event.pageY - mapPos.top;
+  var yPcent = ((yPos / mapWidth) * 100).toPrecision(3);
+
+  //var pageCoords = "( " + xPos + ", " + yPos + " )";
+  var pageCoords = "( " + xPcent + "%, " + yPcent + "% )";
+  $( "span:first" ).text( "( x,y ) : " + pageCoords );
+});
