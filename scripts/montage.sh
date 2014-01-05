ba88ad20163033826169920e52980b793ae41fe0
bryan kennedy <bkennedy@smm.org>
2014-01-04 20:28:30 -0600

    ImageMagick script to make the paleo image montage
    Since it is much faster for the browser to change the image displayed by
    changing the position of a background image we take all of the images
    and montage them together into a long single strip.
    
    This script documents the syntax for using ImageMagick's montage command
    to do that.

---
+++ scripts/montage.sh
@@ -0,0 +1,1 @@
+montage +250.png +200.png +150.png +100.png +050.png +003.png 000.png 005.png 012.png 020.png 035.png 050.png 065.png 075.png 090.png 105.png 120.png 150.png 170.png 200.png 220.png 240.png 260.png 280.png 300.png 340.png 370.png 400.png 420.png 430.png 440.png 450.png 470.png 500.png 540.png 560.png 600.png -tile 37x1 -geometry 1860x930 montage.png
