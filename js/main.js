f960852e3a6f34ad4c0b194f247f50fc58d0f350
bryan kennedy <bkennedy@smm.org>
2014-01-06 16:27:51 -0600

    Switching from tabs to spaces
    I had something borked in my Vim config and was adding spaces instead of
    tabs. Fixed everywhere.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-(function($){
+(function ($) {
 
     // Prevent image dragging
     //
     // Since this is a touch screen experience, we don't want users to be able
     // to touch and drag the main map image around. This prevents dragging.
-	//
-	// TODO - Build this into Backbone
-	document.getElementById('map').ondragstart = function() { return false; };
+    //
+    // TODO - Build this into Backbone
+    document.getElementById('map').ondragstart = function() { return false; };
 
-	/**
-	 * Static Paleogeography images
-	 *
-	 * Maybe these should be broken out in another file?
-	 */
+    /**
+     * Static Paleogeography images
+     *
+     * Maybe these should be broken out in another file?
+     */
     var eras = [
-		{ mya: -250, description: '250 million years in the future' },
+        { mya: -250, description: '250 million years in the future' },
         { mya: -200, description: '200 million years in the future' },
         { mya: -150, description: '150 million years in the future' },
         { mya: -100, description: '100 million years in the future' },
@@ -53,218 +53,216 @@
         { mya: 600, description: 'Later Protoerozoic' },
     ];
 
-	/**
-	 * Models
-	 *
-	 * A single element in the data is an Era.
-	 */
-	var Era = Backbone.Model.extend({
-		defaults: {
-			mya: '',
-			description: '',
-			eraDuration: '',
-			rightBoundary: ''
-		},
-	});
+    /**
+     * Models
+     *
+     * A single element in the data is an Era.
+     */
+    var Era = Backbone.Model.extend({
+        defaults: {
+            mya: '',
+            description: '',
+            eraDuration: '',
+            rightBoundary: ''
+        },
+    });
 
-	/**
-	 * Collections
-	 *
-	 * A grouping of all the Eras represents History
-	 */
-	var History = Backbone.Collection.extend({
-		model: Era
-	});
+    /**
+     * Collections
+     *
+     * A grouping of all the Eras represents History
+     */
+    var History = Backbone.Collection.extend({
+        model: Era
+    });
 
-	/**
-	 * Views
-	 *
-	 * Our primary view is just for looking at the map initially.
-	 */
-	var MapView = Backbone.View.extend({
+    /**
+     * Views
+     *
+     * Our primary view is just for looking at the map initially.
+     */
+    var MapView = Backbone.View.extend({
         el: $('body'),
 
-		// Run the cycleImage function when the main image is clicked.
-		// Eventually we'll add a click and drag event.
-		events: {
-			'click #map': 'cycleImage',
-			'mousemove': 'mousemove',
-		},
+        // Run the cycleImage function when the main image is clicked.
+        // Eventually we'll add a click and drag event.
+        events: {
+            'click #map': 'cycleImage',
+            'mousemove': 'mousemove',
+        },
 
-		initialize: function() {
+        initialize: function() {
 
-			_.bindAll(this, 'render', 'cycleImage');
+            _.bindAll(this, 'render', 'cycleImage');
 
-			// Add the collection to the this object.
-			this.collection = new History(eras);
+            // Add the collection to the this object.
+            this.collection = new History(eras);
 
-			// Get some details about the map that we only need to get once.
-			this.map = $("#map");
-			this.mapPos = this.map.position();
-			//console.log('Map Position', this.mapPos);
-			this.mapWidth = $("#map").innerWidth();
-			this.mapHeight = $("#map").innerHeight();
+            // Get some details about the map that we only need to get once.
+            this.map = $("#map");
+            this.mapPos = this.map.position();
+            //console.log('Map Position', this.mapPos);
+            this.mapWidth = $("#map").innerWidth();
+            this.mapHeight = $("#map").innerHeight();
 
-			// Run the render function. I think this is part of the pattern
-			// convention.
-			this.render();
-		},
+            // Run the render function. I think this is part of the pattern
+            // convention.
+            this.render();
+        },
 
-		// Render the initial state of the map.
-		render: function(){
+        // Render the initial state of the map.
+        render: function(){
 
-			/**
-			 * Build the timeline
-			 */
+            /**
+             * Build the timeline
+             */
 
-			// Initialize a Color object from the color.js library.
-			// This will be the base color for the timeline.
-			var baseColor = Color("#300006");
+            // Initialize a Color object from the color.js library.
+            // This will be the base color for the timeline.
+            var baseColor = Color("#300006");
 
-			// Number of eras in our collection
-			var numEras = this.collection.models.length;
+            // Number of eras in our collection
+            var numEras = this.collection.models.length;
 
-			// Full duration of the time series
-			var startMya = _.first(this.collection.models).attributes.mya;
-			var endMya = _.last(this.collection.models).attributes.mya;
-			// The final period *starts* in the year that it identifies.
-			// Since we want to display it as having a duration itself We
-			// apply a arbitrary duration. This is just a visual display since
-			// technically this period last back all the way into the Cambrian
-			var finalPeriodDuration = 30;
-			var fullDuration = Math.abs(startMya) + endMya +
-				finalPeriodDuration;
-			var fullWidth = 1860;
-			//console.log('Full duration', fullDuration);
+            // Full duration of the time series
+            var startMya = _.first(this.collection.models).get( 'mya' );
+            var endMya = _.last(this.collection.models).get( 'mya' );
+            // The final period *starts* in the year that it identifies.
+            // Since we want to display it as having a duration itself We
+            // apply a arbitrary duration. This is just a visual display since
+            // technically this period last back all the way into the Cambrian
+            var finalPeriodDuration = 30;
+            var fullDuration = Math.abs(startMya) + endMya +
+                finalPeriodDuration;
+            var fullWidth = 1860;
+            //console.log('Full duration', fullDuration);
 
-			// Build the timeline from the eras in our collection
-			_.each(this.collection.models, function(model, i, list){
+            // Build the timeline from the eras in our collection
+            _.each(this.collection.models, function(model, i, list){
 
-				// Append divs for the timeline
-				eraClass = 'era-' + i;
-				var $newDiv = $("<div/>")
-					.addClass('era')
-					.addClass(eraClass)
-					.html('&nbsp;');
-				$('#timeline').append($newDiv);
+                // Append divs for the timeline
+                eraClass = 'era-' + i;
+                var $newDiv = $("<div/>")
+                    .addClass('era')
+                    .addClass(eraClass)
+                    .html('&nbsp;');
+                $('#timeline').append($newDiv);
 
-				// Find the length of each era.
-				// Treat the last one as an arbitrary length.
-				currentMya = model.get('mya');
-				if (i != (numEras - 1)) {
-					nextMya = list[i + 1].get('mya');
-					// Add the duration value to the model
-					model.attributes.eraDuration = nextMya - currentMya;
-				}
-				else {
-					// TODO Put this in as a constant up top
-					model.attributes.eraDuration = 10;
-				}
+                // Find the length of each era.
+                // Treat the last one as an arbitrary length.
+                currentMya = model.get('mya');
+                if (i != (numEras - 1)) {
+                    nextMya = list[i + 1].get('mya');
+                    // Add the duration value to the model
+                    model.attributes.eraDuration = nextMya - currentMya;
+                }
+                else {
+                    // TODO Put this in as a constant up top
+                    model.attributes.eraDuration = 10;
+                }
 
-				// Calculate the right boundaries based on the durations
-				eraWidth = Math.round((
-					model.attributes.eraDuration / fullDuration) * fullWidth);
-				//console.log('ID = ' + model.cid +
-							//', description = ' + model.get('description') +
-							//', width = ' + eraWidth);
+                // Calculate the right boundaries based on the durations
+                eraWidth = Math.round((
+                    model.attributes.eraDuration / fullDuration) * fullWidth);
+                //console.log('ID = ' + model.cid +
+                            //', description = ' + model.get('description') +
+                            //', width = ' + eraWidth);
 
-				if (i === 0) {
-					model.attributes.rightBoundary = eraWidth;
-				}
-				else if (i!= (numEras - 1)) {
-					model.attributes.rightBoundary =
-						list[i - 1].attributes.rightBoundary + eraWidth;
-				}
-				// JS and CSS math is terrible
-				// We just have to tell it that the end is here
-				else {
-					model.attributes.rightBoundary = 1860;
-				}
+                if (i === 0) {
+                    model.attributes.rightBoundary = eraWidth;
+                }
+                else if (i!= (numEras - 1)) {
+                    model.attributes.rightBoundary =
+                        list[i - 1].attributes.rightBoundary + eraWidth;
+                }
+                // JS and CSS math is terrible
+                // We just have to tell it that the end is here
+                else {
+                    model.attributes.rightBoundary = 1860;
+                }
 
-				// Store the interval in the model so that we can reference
-				// different eras in events
-				model.attributes.interval = i;
+                // Store the interval in the model so that we can reference
+                // different eras in events
+                model.attributes.interval = i;
 
-				// Add width
+                // Add width
 
-				//console.log(eraWidth);
-				$('.' + eraClass).css('width', eraWidth + 'px');
+                //console.log(eraWidth);
+                $('.' + eraClass).css('width', eraWidth + 'px');
 
-				// Add color
-				colorModifier = (1 / (i + 1)) * 10;
-				periodColor = baseColor.lighten( 0.07 );
-				baseColor = Color(periodColor.hexString());
-				$('.' + eraClass).css("background", periodColor.hexString());
+                // Add color
+                colorModifier = (1 / (i + 1)) * 10;
+                periodColor = baseColor.lighten( 0.07 );
+                baseColor = Color(periodColor.hexString());
+                $('.' + eraClass).css("background", periodColor.hexString());
 
-			});
+            });
 
-			// Debug check
-			//_.each(this.collection.models, function(model, i, list){
-				//console.log('Mya: ' + model.get('mya') +
-							////'Desc.: ' + model.get('description') +
-							//', Right bound: ' + model.get('rightBoundary'));
-			//});
+            // Debug check
+            //_.each(this.collection.models, function(model, i, list){
+                //console.log('Mya: ' + model.get('mya') +
+                            ////'Desc.: ' + model.get('description') +
+                            //', Right bound: ' + model.get('rightBoundary'));
+            //});
 
-		},
+        },
 
-		// TODO - In the future, do click checking
-		cycleImage: function () {
-			console.log('Click');
-		},
+        // TODO - In the future, do click checking
+        cycleImage: function () {
+            console.log('Click');
+        },
 
-		mousemove: function(e) {
+        mousemove: function(e) {
 
-			// Get mouse position
-			xPos = event.pageX - this.mapPos.left;
-			yPos = event.pageX - this.mapPos.top;
-			//console.log("Mouse coordinates", xPos + ", " + yPos);
+            // Get mouse position
+            xPos = event.pageX - this.mapPos.left;
+            yPos = event.pageX - this.mapPos.top;
+            //console.log("Mouse coordinates", xPos + ", " + yPos);
 
-			// Turn it into a percentage of the map width
-			xPcent = ((xPos / this.mapWidth) * 100);
-			yPcent = ((yPos / this.mapHeight) * 100);
-			mousePos = xPos.toFixed(2) + 'px, ' + yPos.toFixed(2) + 'px';
-			//console.log('Mouse pos', mousePos);
+            // For debugging
+            mousePos = xPos.toFixed(2) + 'px, ' + yPos.toFixed(2) + 'px';
+            //console.log('Mouse pos', mousePos);
 
-			// Get the model for the era that we are hovering over.
-			currentEra = this.collection.filter(function(model) {
-				if (xPos <= model.get('rightBoundary')){
-					return true;
-				}
-			});
-			currentEra = _.first(currentEra);
+            // Get the model for the era that we are hovering over.
+            currentEra = this.collection.filter(function(model) {
+                if (xPos <= model.get('rightBoundary')){
+                    return true;
+                }
+            });
+            currentEra = _.first(currentEra);
 
-			// Move the background image "interval" numbers of times
-			// TODO var the pixel width
-			var interval = currentEra.get('interval');
-			if (interval !== 0) {
-				intervalMultiplier = interval * -1860;
-			}
-			else {
-				intervalMultiplier = 0;
-			}
+            // Move the background image "interval" numbers of times
+            // TODO var the pixel width
+            var interval = currentEra.get('interval');
+            if (interval !== 0) {
+                intervalMultiplier = interval * -1860;
+            }
+            else {
+                intervalMultiplier = 0;
+            }
 
-			// Set the current image based on the mouse position interval
-			currentMya = currentEra.get('mya');
-			currentPeriod = currentEra.get('description');
+            // Set the current image based on the mouse position interval
+            currentMya = currentEra.get('mya');
+            currentPeriod = currentEra.get('description');
 
-			// We use css to change the image since it's lots faster than
-			// doing this with file replacement. Even locally.
-			$('#map').css('background-position', intervalMultiplier);
+            // We use css to change the image since it's lots faster than
+            // doing this with file replacement. Even locally.
+            $('#map').css('background-position', intervalMultiplier);
 
-			// Indicate our current frame in the timeline
-			$(".era").removeClass("era-active");
-			$('.era-' + interval).addClass('era-active');
+            // Indicate our current frame in the timeline
+            $(".era").removeClass("era-active");
+            $('.era-' + interval).addClass('era-active');
 
-			// Display debug information
-			$('#info').html(' - Mouse: ' + mousePos +
-							', Interval: ' + interval +
-							', Mya: ' + currentMya +
-							', Period: ' + currentPeriod +
-							', intervalMultiplier: ' + intervalMultiplier);
-		}
+            // Display debug information
+            $('#info').html(' - Mouse: ' + mousePos +
+                            ', Interval: ' + interval +
+                            ', Mya: ' + currentMya +
+                            ', Period: ' + currentPeriod +
+                            ', intervalMultiplier: ' + intervalMultiplier);
+        }
 
-	});
+    });
 
-	var mapView = new MapView();
+    var mapView = new MapView();
 
 })(jQuery);
